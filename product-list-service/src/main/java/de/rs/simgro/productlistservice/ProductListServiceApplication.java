8f8da245a586b2e8a74466aedc59cbb6261d907a
Rene Sommerfeld <rene.t.sommerfeld@gmail.com>
2020-03-22 16:53:20 +0100

    [product-list-service] initial setup

---
+++ product-list-service/src/main/java/de/rs/simgro/productlistservice/ProductListServiceApplication.java
@@ -0,0 +1,13 @@
+package de.rs.simgro.productlistservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductListServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductListServiceApplication.class, args);
+	}
+
+}
