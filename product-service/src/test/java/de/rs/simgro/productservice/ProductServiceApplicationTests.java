90e1e4dd3971afe2e955f9c60f2f8aaa16e64ad4
Rene Sommerfeld <rene.t.sommerfeld@gmail.com>
2020-03-22 15:23:26 +0100

    [product-service] initial setup

---
+++ product-service/src/test/java/de/rs/simgro/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rs.simgro.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
