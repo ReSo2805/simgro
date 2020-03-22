e9b6f549b67da60934f354c9bf7303a1451b2350
Rene Sommerfeld <rene.t.sommerfeld@gmail.com>
2020-03-22 16:39:16 +0100

    [product-stock-service] initial setup

---
+++ product-stock-service/src/main/java/de/rs/simgro/productstockservice/ProductStockServiceApplication.java
@@ -0,0 +1,13 @@
+package de.rs.simgro.productstockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStockServiceApplication.class, args);
+	}
+
+}
