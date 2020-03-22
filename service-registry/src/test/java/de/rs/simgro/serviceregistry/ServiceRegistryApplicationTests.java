14968b6d3f800bf6f6efe61945b9793c412b5f28
Rene Sommerfeld <rene.t.sommerfeld@gmail.com>
2020-03-22 14:49:05 +0100

    [service-registry] initial setup

---
+++ service-registry/src/test/java/de/rs/simgro/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rs.simgro.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
